37ae0580f1a6b102001b0da10699f1546d88c11d
S.A <thedevdiary0@gmail.com>
2023-10-01 23:01:38 +0200

    setting up Program.cs

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -53,12 +53,6 @@ namespace webapi.Data
 
             app.Run();
         }
-
-        // EF Core uses this method at design time to access the DbContext
-        public static IHostBuilder CreateHostBuilder(string[] args)
-            => Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(
-                    webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
 
